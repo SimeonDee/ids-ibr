d8e157af08ae5607e6f8f2a31b50a9b3becc3ea7
SimeonDee <adeyemi.adedoyin.simeon@gmail.com>
2026-01-28 14:03:07 +0100

    sample test results to use

--- inference_app/inference_app.py
+++ inference_app/inference_app.py
@@ -106,11 +106,12 @@ if "result" not in st.session_state:
 st.write(
     dedent(
         """
-    ### Design and Implementation of a Machine Learning-based \
+    >#### <center>Design and Implementation of a Machine Learning-based \
     Intrusion Detection System using KNN, SVM and Random \
-    Forest to Classify Network Traffic as Normal or Abnormal
+    Forest to Classify Network Traffic as Normal or Abnormal</center>
 """
-    )
+    ),
+    unsafe_allow_html=True,
 )
 
 with st.sidebar:
@@ -120,105 +121,118 @@ with st.sidebar:
     print(f"Model Name: {model_name}")
     model = load_model(model_name)
 
-with st.container(border=True):
-    protocol_type = st.selectbox(
-        "Protocol Type",
-        options=PROTOCOL_DATA,
-        index=0,
-    )
-    service = st.selectbox("Service", options=SERVICE_SELECTION_DATA, index=0)
-    flag = st.selectbox("Flag", options=FLAG_DATA, index=1)
-    src_bytes = st.number_input("Source Bytes", min_value=0)
-    dest_bytes = st.number_input("Destination Bytes", min_value=0)
-    msg_count = st.number_input(
-        "Message Count",
-        min_value=1,
-        max_value=600,
-        value=2,
-    )
-    same_srv_rate = st.slider(
-        "Same Server Rate",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.5,
-    )
-    diff_srv_rate = st.slider(
-        "Different Server Rate",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.5,
-    )
-    dst_host_srv_count = st.number_input(
-        "Destination Host Server Count", min_value=0, max_value=255, value=255
-    )
-    dst_host_same_srv_rate = st.slider(
-        "Destination Host Same Server Rate",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.5,
-    )
-    dst_host_same_src_port_rate = st.slider(
-        "Destination Host Same Source Port Rate",
-        min_value=0.0,
-        max_value=1.0,
-        value=0.5,
-    )
 
 with st.container(border=True):
-    predict_button = st.button(
-        "Predict",
-        type="primary",
-        use_container_width=True,
-    )
+    st.markdown("##### Input Network Traffic Features")
+    st.write("---")
+    col1, col2 = st.columns([0.5, 0.5])
 
-    if predict_button:
-        data = np.array(
-            [
-                # PROTOCOL_DATA.index(protocol_type),
-                SERVICE_SELECTION_DATA.index(service),
-                FLAG_DATA.index(flag),
-                int(src_bytes),
-                int(dest_bytes),
-                int(msg_count),
-                float(same_srv_rate),
-                float(diff_srv_rate),
-                int(dst_host_srv_count),
-                float(dst_host_same_srv_rate),
-                float(dst_host_same_src_port_rate),
-            ]
+    with col1:
+        protocol_type = st.selectbox(
+            "Protocol Type",
+            options=PROTOCOL_DATA,
+            index=0,
         )
-        scaled_data = scaler.transform(data.reshape(1, -1))
-        predicted_label = model.predict(scaled_data)[0]
-        predicted_value = REVERSE_ENCODINGS["class"][predicted_label]
-        # data_types = [type(val) for val in data]
+        service = st.selectbox("Service", options=SERVICE_SELECTION_DATA, index=0)
+        flag = st.selectbox("Flag", options=FLAG_DATA, index=1)
+        src_bytes = st.number_input("Source Bytes", min_value=0)
+        dest_bytes = st.number_input("Destination Bytes", min_value=0)
+        msg_count = st.number_input(
+            "Message Count",
+            min_value=1,
+            max_value=600,
+            value=2,
+        )
 
-        st.session_state.model = model_name
-        st.session_state.result = predicted_value.upper()
-        color = (
-            "rgb(60,220, 60)"  # green for normal
-            if "normal" in st.session_state.result.lower()
-            else "rgb(250, 100, 100)"  # no-qa red for Anomaly
-        )  # no-qa
-        displayed_result = (
-            "<span "
-            f"style='color: {color}; "
-            "font-weight: bold;'>"
-            f"{st.session_state.result}"
-            "</span>"
-        )  # no-qa
+    with col2:
+        same_srv_rate = st.slider(
+            "Same Server Rate",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+        )
+        diff_srv_rate = st.slider(
+            "Different Server Rate",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+        )
+        dst_host_srv_count = st.number_input(
+            "Destination Host Server Count", min_value=0, max_value=255, value=255
+        )
+        dst_host_same_srv_rate = st.slider(
+            "Destination Host Same Server Rate",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+        )
+        dst_host_same_src_port_rate = st.slider(
+            "Destination Host Same Source Port Rate",
+            min_value=0.0,
+            max_value=1.0,
+            value=0.5,
+        )
+
         with st.container(border=True):
-            st.write("### Prediction Result")
-            st.write("---")
-            st.markdown(
-                "<center><span style='font-weight: bold; font-size:1.5rem'>"
-                # "Prediction | "
-                f"{displayed_result}</span></center>" "<hr />",
-                unsafe_allow_html=True,
+            predict_button = st.button(
+                "Predict",
+                type="primary",
+                use_container_width=True,
             )
-            # st.write(f"Prediction: {predicted_value}")
+
+            if predict_button:
+                data = np.array(
+                    [
+                        # PROTOCOL_DATA.index(protocol_type),
+                        SERVICE_SELECTION_DATA.index(service),
+                        FLAG_DATA.index(flag),
+                        int(src_bytes),
+                        int(dest_bytes),
+                        int(msg_count),
+                        float(same_srv_rate),
+                        float(diff_srv_rate),
+                        int(dst_host_srv_count),
+                        float(dst_host_same_srv_rate),
+                        float(dst_host_same_src_port_rate),
+                    ]
+                )
+                scaled_data = scaler.transform(data.reshape(1, -1))
+                predicted_label = model.predict(scaled_data)[0]
+                predicted_value = REVERSE_ENCODINGS["class"][predicted_label]
+                # data_types = [type(val) for val in data]
+
+                st.session_state.model = model_name
+                st.session_state.result = predicted_value.upper()
+                color = (
+                    "rgb(60,220, 60)"  # green for normal
+                    if "normal" in st.session_state.result.lower()
+                    else "rgb(250, 100, 100)"  # no-qa red for Anomaly
+                )  # no-qa
 
 
-if st.session_state.result:
+if st.session_state.result and st.session_state.model:
+    color = (
+        "rgb(60,220, 60)"  # green for normal
+        if "normal" in st.session_state.result.lower()
+        else "rgb(250, 100, 100)"  # no-qa red for Anomaly
+    )  # no-qa
+    displayed_result = (
+        "<span "
+        f"style='color: {color}; "
+        "font-weight: bold;'>"
+        f"{st.session_state.result}"
+        "</span>"
+    )  # no-qa
+    with st.container(border=True):
+        st.write("##### Prediction Result")
+        st.write("---")
+        st.markdown(
+            "<center><span style='font-weight: bold; font-size:1.5rem'>"
+            f"{displayed_result}</span></center>"
+            "<hr />",
+            unsafe_allow_html=True,
+        )
+
     with st.sidebar:
         with st.container(border=True):
             st.write("# Prediction Results\n---\n")
